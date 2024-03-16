b2c252af1986480ef2c873235bf09d88420207a4
Nayan1234l <ali15-13943@diu.edu.bd>
2024-03-16 23:40:25 +0600

    max-ones

---
+++ Max-consecutive-ones.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let i = 0;
+    let j = 0;
+    let zeroCount = 0;
+    let result = 0;
+    for(i = 0; i<nums.length; i++){
+        if(nums[i]===0){
+            zeroCount++;
+        }
+        while(zeroCount>k){
+               if(nums[j]===0) zeroCount--;  
+               j++
+            
+        }
+        result = Math.max(result,i-j+1)
+    }
+    return result;
+};
