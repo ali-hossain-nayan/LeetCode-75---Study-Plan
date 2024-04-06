2c3459f807e9dfebf7e59c5f03fa7427c4e94324
Nayan1234l <ali15-13943@diu.edu.bd>
2024-04-06 12:10:03 +0600

    rsv-solved

---
+++ BSTrightSideView.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+    const rsv = [];
+
+    const dfs = (node, depth) => {
+        if (!node) return;
+
+        if (depth >= rsv.length) {
+            rsv.push(node.val);
+        }
+
+        dfs(node.right, depth + 1);
+        dfs(node.left, depth + 1);
+    }
+
+    dfs(root, 0);
+
+    return rsv;
+};
