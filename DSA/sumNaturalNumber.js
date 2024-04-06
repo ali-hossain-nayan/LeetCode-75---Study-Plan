cc00ead74ed6ff0fa5725dd850d73bda23cf2679
Nayan1234l <ali15-13943@diu.edu.bd>
2024-04-06 12:44:20 +0600

    updated

--- DSA/sumNaturalNumber.js
+++ DSA/sumNaturalNumber.js
@@ -1,10 +1,12 @@
 
-const sumNaturalNumber = (num) => {
+const sumNaturalNumber = (n) => {
     let ans = 0
 
-    for (let i = 1; i <= num; i++) {
+    for (let i = 1; i <= n; i++) {
         ans += i;
     }
     return ans;
+
+    // return n*(n+1)/2
 }
-// console.log(sumNaturalNumber(12))
+console.log(sumNaturalNumber(12))
