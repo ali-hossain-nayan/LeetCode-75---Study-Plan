3b7cd304f4fd28c889f6ca41e4a815b2570643aa
Ali Hossain Nayan <94155479+Nayan1234l@users.noreply.github.com>
2024-04-13 12:29:47 +0600

    minimum-cost-to-climb-top-stairs

---
+++ DSA/MinCostClimbing.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+    let first = cost[0], second = cost[1];
+    let temp;
+    for (let i = 2; i < cost.length; i++) {
+        temp = cost[i] + Math.min(first, second);
+        first = second;
+        second = temp;
+    }
+    return Math.min(first, second);
+};
