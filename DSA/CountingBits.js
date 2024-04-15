ccb1545886ef72d4ab190b35df045de5f3906f6c
Ali Hossain Nayan <94155479+Nayan1234l@users.noreply.github.com>
2024-04-15 10:25:34 +0600

    counting-1's-bit

---
+++ DSA/CountingBits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function (n) {
+    let ans = [0];
+    for (let i = 1; i <= n; i++) {
+        ans[i] = ans[Math.floor(i / 2)] + i % 2;
+
+    }
+    return ans
+};
