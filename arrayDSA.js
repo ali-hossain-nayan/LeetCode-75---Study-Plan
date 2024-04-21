9e8e362c55014b4ab0dda9b786e0fa99c2a0baaf
Ali Hossain Nayan <94155479+Nayan1234l@users.noreply.github.com>
2024-04-21 22:17:27 +0600

    flat-filter-vs-find-method

--- arrayDSA.js
+++ arrayDSA.js
@@ -20,14 +20,14 @@ arr.forEach((i) => {
 for (let i of arr) {
     // console.log(i)
 }
-// console.log(arr)
+console.log(arr)
 //delete,add,update element using splice
 
 // arr.splice(1,3)
 // console.log(arr)
 
-// arr.splice(1,0,6,7,8)
-// console.log(arr)
+arr.splice(1,0,6,7,8)
+console.log(arr)
 
 // arr.splice(1,3,0,9,8)
 // console.log(arr)
@@ -78,5 +78,14 @@ const newFilter = arrA.filter((ele, index) => ele > 0)
 const newReduce = arrA.reduce((acc, cul) => acc + cul, 0)
 // console.log(newReduce)
 
+//flat
+const ar = [2,-3,-4,[3,6,7,[3,4,7]]]
+const FlatArray = ar.flat(3)//for all depth we can use Infinity.
+console.log(FlatArray)
+//filter vs find
+//filter find all element that will match its condition but find is work only for first element 
+//if its find the first element with condition and return it not check rest of the array.
+const positiveInt = ar.find((ele)=>ele>0)
+console.log(positiveInt)
 
 
