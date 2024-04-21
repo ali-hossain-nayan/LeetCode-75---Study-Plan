a60afbb45b02b02e3036e711b8df609730a83cda
Ali Hossain Nayan <94155479+Nayan1234l@users.noreply.github.com>
2024-04-21 14:48:53 +0600

    dsa-array-loop

---
+++ arrayDSA.js
@@ -0,0 +1,22 @@
+const arr = [2, 4, 'nayan', { name: 'ali' }, false]
+
+// const lastElement = console.log(arr[arr.length-1])
+//first insert and delete
+// console.log(arr.shift())
+// console.log(arr.unshift())
+
+//last push and pop
+// console.log(arr.push(7))
+// console.log(arr.pop())
+
+for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i])
+}
+
+arr.forEach((i) => {
+    // console.log(i)
+})
+
+for(let i of arr){
+    // console.log(i)
+}
