f87c074a80882d3b8cb90b1d5cca7a42a80fad1a
Ali Hossain Nayan <94155479+Nayan1234l@users.noreply.github.com>
2024-04-21 15:37:56 +0600

    updated

--- arrayDSA.js
+++ arrayDSA.js
@@ -20,3 +20,44 @@ arr.forEach((i) => {
 for(let i of arr){
     // console.log(i)
 }
+// console.log(arr)
+//delete,add,update element using splice
+
+// arr.splice(1,3)
+// console.log(arr)
+
+// arr.splice(1,0,6,7,8)
+// console.log(arr)
+
+// arr.splice(1,3,0,9,8)
+// console.log(arr)
+// console.log(arr)
+
+//not changes main array 
+// const arrsli = arr.slice(1,3)
+// console.log(arrsli)
+// console.log(arr)
+
+//changes main array
+// const arrspli = arr.splice(1,3)
+// console.log(arrspli)
+// console.log(arr)
+
+//shallow copy changes in main array. it's not good practice
+// console.log(arr)
+// const arr1 = arr;
+// arr1.splice(1,3)
+// console.log(arr)
+
+//deep copy not changes in main array.different ways we can do it
+console.log(arr)
+const arr2 = [...arr]
+const arr3 = [Array.from(arr)]
+const arr4 = arr.concat()
+console.log(arr2,arr3,arr4)
+
+
+
+
+
+
