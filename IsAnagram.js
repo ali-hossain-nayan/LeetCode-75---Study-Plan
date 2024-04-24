85b687495571fa3b625e4c5ff0458f843a3c6ea8
Nayan1234l <ali15-13943@diu.edu.bd>
2024-04-24 09:22:40 +0600

    compare-string-anagram-solved

---
+++ IsAnagram.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) {
+        return false
+    }
+    let sortS = s.split('').sort().join('')
+    let sortT = t.split('').sort().join('')
+
+    // for(let i=0;i<s;i++){
+    //    for(let j=0;j<t;j++){
+    //     if(s[i]!==t[j]){
+    //         return false
+    //     }
+    //    }
+    // }
+    return sortT.includes(sortS)
+};
