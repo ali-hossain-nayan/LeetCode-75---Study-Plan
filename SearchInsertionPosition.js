fbc094336cabd596a4e55865126f473af2ec2847
Nayan1234l <ali15-13943@diu.edu.bd>
2024-04-27 12:37:27 +0600

    search-insertion-positon-using-binary-search

---
+++ SearchInsertionPosition.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[mid] === target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+    return left
+
+};
