b1d88cd89e77fac92e81a1a0024a4d7a43b9be50
Nayan1234l <ali15-13943@diu.edu.bd>
2024-04-29 15:14:52 +0600

    updated

--- GroupAnagram.js
+++ GroupAnagram.js
@@ -18,3 +18,24 @@ var groupAnagrams = function (strs) {
     return Object.values(groupAnagram)
 
 };
+
+
+
+
+
+const Anagram = (strs) => {
+    let gpAnagram = {}
+    for (let i = 0; i < strs.length; i++) {
+
+        let anagrams = strs.split('').sort().join('');
+        if(!gpAnagram.hasOwnProperty(anagrams)){//check the key already exists in the gpAnagram
+               gpAnagram[anagrams]=[]//if doesnt then assign a empty array
+        }
+        gpAnagram[anagrams].push(strs[i])//and the push create anagrams into the anagram key 
+
+        
+    }
+
+    return Object.values(gpAnagram)//return array of value from the object
+
+}
