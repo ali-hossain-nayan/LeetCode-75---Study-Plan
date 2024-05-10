ff147f76619bace66c1cd8adb75e589f218c920d
Nayan1234l <ali15-13943@diu.edu.bd>
2024-05-10 18:20:49 +0600

    another-approach-updated-to-solve-it

--- sortColors.js
+++ sortColors.js
@@ -3,5 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    nums.sort((a, b) => a - b)
+    // nums.sort((a, b) => a - b)
+    //another approach
+    for (let i = 0; i < nums.length - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] < nums[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]]
+        }
+    }
 };
